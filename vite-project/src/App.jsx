648e0c9c08b9f43ed910597d2d56c7b138a26b69
Suraj <svsurajshailendra@gmail.com>
2026-02-21 12:15:44 +0530

    Deplyped

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [uimg,setuimg]=useState("");
   const [aname,setaname]=useState("");
     const sign=async ()=>{
-      const api=await fetch("http://localhost:3000/signup",{
+      const api=await fetch("https://mybackend-zwr6.onrender.com/signup",{
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify({name,email,password,image})
@@ -24,7 +24,7 @@ function App() {
     }
 
     const log=async ()=>{
-      const api=await fetch("http://localhost:3000/login",{
+      const api=await fetch("https://mybackend-zwr6.onrender.com/login",{
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body:JSON.stringify({email:lemail,password:lpassword})
@@ -47,14 +47,14 @@ function App() {
          <input placeholder='Enter email' value={email} onChange={(e)=>{setemail(e.target.value)}}></input>
          <input placeholder='Enter password' value={password} onChange={(e)=>{setpassword(e.target.value)}}></input>
          <input placeholder='Paste Image Path' value={image} onChange={(e)=>{setimage(e.target.value)}}></input>
-         <button type='submit' onClick={sign}>Signup</button>
+         <button type='button' onClick={sign}>Signup</button>
         </div>
         
      <div className="loginform">
       <h2 id='title'>Login</h2>
         <input placeholder='Enter email' value={lemail} onChange={(e)=>{setlemail(e.target.value)}}></input>
         <input placeholder='Enter password' value={lpassword} onChange={(e)=>{setlpassword(e.target.value)}}></input>
-        <button type='submit' onClick={log}>Login</button>
+        <button type='button' onClick={log}>Login</button>
        {isLoggedIn && <div>
         <h3>Welcome</h3>
         <img src={uimg} alt="User" width="60" height="60" style={{border:"3px solid white",borderRadius:"30px"}} />
