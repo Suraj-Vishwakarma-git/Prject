4a7f54c2fcf535067bf3c17472b17567761bc153
Suraj <svsurajshailendra@gmail.com>
2026-02-21 09:15:36 +0530

    Edited

--- pro.js
+++ pro.js
@@ -5,10 +5,52 @@ import cors from "cors";
 
 const server=express();
 server.use(express.json());
+server.use(cors());
 
-server.get("/",(req,res)=>{
-    res.json("Alert data used");
-})
+await mongoose.connect("mongodb://127.0.0.1:27017/myapp");
+console.log("MongoDB connected");
+
+const User=mongoose.model("User",new mongoose.Schema({
+    name:String,
+    email:{type:String,unique:true},
+    password:String,
+    image:String
+}));
+
+server.post("/signup",async (req,res)=>{
+   try{const {name,email,password,image}=req.body;
+   if(!name || !email || !password || !image){
+    return res.json({message:"All fields re required"});
+   }
+   const hashed=await bcrypt.hash(password,10);
+   const user=await User.create({
+    name,
+    email,
+    password:hashed,
+    image
+   });
+  res.json({
+    message:"Account created",
+    Account:user
+  })
+}
+catch(e){
+    console.log(e);
+}});
+
+server.post("/login",async (req,res)=>{
+   const {email,password}=req.body;
+   if( !email || !password){
+    return res.json({message:"All fields re required"});
+   }
+   const user=await User.findOne({email});
+   if (!user) {
+      return res.json({ message: "User not found" });
+    }
+   const cform=await bcrypt.compare(password,user.password);
+   if(!cform) return res.json({message:"Invalid password"});
+   res.json({message:"Loged in successfully" ,Account:user.image});
+});
 
 server.listen(3000,()=>{
     console.log("SErver started");
